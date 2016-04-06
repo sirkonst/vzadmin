f2566ebee50e770aacd06c5c0ef351406f6f0751
Konstantin vz'One Enchant <sirkonst@gmail.com>
2016-04-06 10:43:00 +0300

    Refact and translate to English

--- vzadmin.sh
+++ vzadmin.sh
@@ -3,9 +3,15 @@
 #------------------------------------------------------------------------------
 
 ## Config
-DG=/usr/bin/dialog
-VZC=/usr/sbin/vzctl
-VZL=/usr/sbin/vzlist
+DG=`which dialog`
+VZC=`which vzctl`
+VZL=`which vzlist`
+
+if [[ -z "$DG" || -z "$VZC" || -z "$VZL" ]]; then
+    echo "[!] Ensure that installed: dialog, vzctl and vzlist"
+    exit 1
+fi
+
 
 ## temp file
 tempfile=`mktemp 2>/dev/null` || tempfile=/tmp/test$$
@@ -13,62 +19,49 @@ trap "rm -f $tempfile" 0 1 2 5 15
 
 #------------------------------------------------------------------------------
 
-## Меню - входа в VPS
+
 _menu_enter() {
-	CTLIST=$(printf "%s\\%s [ip:%s]\\" `$VZL -H -o ctid,hostname,ip`)
+    CTLIST=$(printf "%s\\%s [ip:%s]\\" `$VZL -H -o ctid,hostname,ip`)
 
-	IFS='\'
+    IFS='\'
 
-	$DG --clear --backtitle "OpenVZ Control Panel" --title "Список запущенных VPS" \
-		--ok-label "Выбрать" --cancel-label "Выход" \
-		--menu "Выберите VPS:" 0 0 0 \
-		"" "Вывести cписок всех VPS ->" $CTLIST 2> $tempfile
+    $DG --clear --backtitle "OpenVZ Control Panel" --title "List of running VPS" \
+        --ok-label "Select" --cancel-label "Exit" \
+        --menu "Select VPS:" 0 0 0 \
+        "" "Display all VPS ->" $CTLIST 2> $tempfile
 
-	retval=$?
+    retval=$?
 
-	unset IFS
+    unset IFS
 
-	choice=`cat $tempfile`
+    choice=`cat $tempfile`
 
-	if [ $retval == 0 ] ; then
-		if [ -z $choice ]; then
-			_menu_list
-		else
-			$VZC enter $choice
-		fi
-	else
-		exit
-	fi
+    if [ $retval == 0 ] ; then
+            if [ -z $choice ]; then
+                    _menu_list
+            else
+                    $VZC enter $choice
+            fi
+    else
+            exit
+    fi
 }
 
-# Меню - список VPS
+
 _menu_list() {
-	CTLIST=$(printf "%s\\%s [%s]\\" `$VZL -a -H -o ctid,hostname,status`)
+    CTLIST=$(printf "%s\\%s [%s]\\" `$VZL -a -H -o ctid,hostname,status`)
 
-	IFS='\'
+    IFS='\'
 
-	$DG --clear --backtitle "OpenVZ Control Panel" --title "Список всех VPS" \
-		--nocancel --ok-label "Назад" \
-		--menu "Список всех VPS:" 0 0 0 \
-		$CTLIST 2> $tempfile
+    $DG --clear --backtitle "OpenVZ Control Panel" --title "List of all VPS" \
+            --nocancel --ok-label "Go back" \
+            --menu "List of all VPS:" 0 0 0 \
+            $CTLIST 2> $tempfile
 
-#	retval=$?
-
-	unset IFS
-
-#	choice=`cat $tempfile`
-
-# 	if [ $retval == 0 ] ; then
-# 		if [ -z $choice ]; then
-# 			_menu_list
-# 		else
-# 			$VZC enter $choice
-# 		fi
-# 	else
-# 		exit
-# 	fi
+    unset IFS
 }
+
 
 while true; do
-	_menu_enter
+    _menu_enter
 done
